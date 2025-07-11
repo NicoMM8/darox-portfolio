3eb1eb88e8350cc8c7bc6fa578f447c2e7e3f4df
NicoMM8 <nmm1020@alu.ubu.es>
2025-07-11 16:59:38 +0200

    Añadida librería para rendimiento

--- darox-portfolio/src/pages/_app.tsx
+++ darox-portfolio/src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../estilos/globals.css'
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
